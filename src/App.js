9750765b5603429ac65bea3b1dea5f6ea58b06f3
Khumbongmayum Tonny <tonnykh@icloud.com>
2023-02-13 19:10:16 +0530

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,13 +3,31 @@ import Head from "./components/Head";
 import Body from "./components/Body";
 import { Provider } from "react-redux";
 import store from "./utils/store";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainContainer from "./components/MainContainer";
+import WatchPage from "./components/WatchPage";
+
+const appRouter = createBrowserRouter([{
+  path: "/",
+  element: <Body />,
+  children: [
+    {
+      path: "/",
+      element: <MainContainer />
+    },
+    {
+      path: "watch",
+      element: <WatchPage />
+    }
+  ]
+}]);
 
 function App() {
   return (
     <Provider store={store}>
       <div>
         <Head />
-        <Body />
+        <RouterProvider router={appRouter} />
         {/**
          *
          *
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,12 @@
 import React from "react";
-import MainContainer from "./MainContainer";
 import Sidebar from "./Sidebar";
+import { Outlet } from "react-router-dom";
 
 const Body = () => {
   return (
     <div className="flex">
       <Sidebar />
-      <MainContainer />
+      <Outlet />
     </div>
   );
 };
