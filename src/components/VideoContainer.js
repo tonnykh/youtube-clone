65c8830efa68f7edc983bbd1db2b8a8c576b10bf
Khumbongmayum Tonny <tonnykh@icloud.com>
2023-02-13 17:33:59 +0530

    Fetch Youtube video list

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { YOUTUBE_VIDEOS_API } from "../utils/constants";
 
 const VideoContainer = () => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    getVideos();
+  }, []);
+
+  const getVideos = async () => {
+    const data = await fetch(YOUTUBE_VIDEOS_API);
+    const json = await data.json();
+    console.log(json);
+
+    setVideos(json);
+  };
+
   return <div>VideoContainer</div>;
 };
 
---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+
+
+const GOOGLE_API_KEY = 'AIzaSyClu2V_22XpCG2GTe1euD35_Mh5bn4eTjA';
+
+export const YOUTUBE_VIDEOS_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=" +
+  GOOGLE_API_KEY;
+ 
