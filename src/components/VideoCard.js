bf70ead4a8e765dc0b2aac29310fa0354d7e4f63
Khumbongmayum Tonny <tonnykh@icloud.com>
2023-02-21 23:13:30 +0530

    Add a numer format helper function

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { numberFormatter } from "../utils/helper";
 
 const VideoCard = ({ info }) => {
   if (info === undefined) return null;
+  console.log(info, "INFO");
 
   const { snippet, statistics } = info;
   const { channelTitle, title, thumbnails } = snippet;
@@ -16,10 +18,10 @@ const VideoCard = ({ info }) => {
       <ul>
         <li className="font-bold py-2 leading-[1.2rem]">{title}</li>
         <li className="text-gray-600 text-sm">{channelTitle}</li>
-        <li
-          className="text-gray-600 text-sm">
-          {statistics.viewCount} views
+        <li className="text-gray-600 text-sm">
+          {numberFormatter.format(statistics.viewCount)} views
         </li>
+        <li className="text-gray-600 text-sm">{snippet.publishedAt}</li>
       </ul>
     </div>
   );
---
+++ src/utils/helper.js
@@ -0,0 +1,6 @@
+export const numberFormatter = new Intl.NumberFormat("en-GB", {
+  notation: "compact",
+  compactDisplay: "short",
+});
+
+// console.log(numberFormatter.format(9876));
